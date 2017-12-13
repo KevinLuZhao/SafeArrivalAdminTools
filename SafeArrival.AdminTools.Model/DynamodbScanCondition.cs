514df700d19d6e82bf5908261de7980993c5eb3b
KevinLuZhao <kevin.luzhao@gmail.com>
2017-12-13 18:00:10 -0500

    Table scan improve

---
+++ SafeArrival.AdminTools.Model/DynamodbScanCondition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeArrival.AdminTools.Model
+{
+    public class DynamodbScanCondition
+    {
+        public string AttributeName { get; set; }
+        //public DynamodbScanOperator Operator { get; set; }
+        public string Operator { get; set; }
+        public object Value { get; set; }
+    }
+}
---
+++ SafeArrival.AdminTools.Model/DynamodbScanOperator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeArrival.AdminTools.Model
+{
+    public class DynamodbScanOperator
+    {
+        //readonly DynamodbScanOperator
+        //
+        // Summary:
+        //     Constant BEGINS_WITH for DynamodbScanOperator
+        public static string BEGINS_WITH;
+        //
+        // Summary:
+        //     Constant BETWEEN for DynamodbScanOperator
+        public static string BETWEEN { get { return "BETWEEN"; } }
+        //
+        // Summary:
+        //     Constant CONTAINS for DynamodbScanOperator
+        public static string CONTAINS { get { return "CONTAINS"; } }
+        //
+        // Summary:
+        //     Constant EQ for DynamodbScanOperator
+        public static string EQ { get { return "EQ"; } }
+        //
+        // Summary:
+        //     Constant GE for DynamodbScanOperator
+        public static string GE;
+        //
+        // Summary:
+        //     Constant GT for DynamodbScanOperator
+        public static string GT;
+        //
+        // Summary:
+        //     Constant IN for DynamodbScanOperator
+        public static string IN;
+        //
+        // Summary:
+        //     Constant LE for DynamodbScanOperator
+        public static string LE;
+        //
+        // Summary:
+        //     Constant LT for DynamodbScanOperator
+        public static string LT;
+        //
+        // Summary:
+        //     Constant NE for DynamodbScanOperator
+        public static string NE;
+        //
+        // Summary:
+        //     Constant NOT_CONTAINS for DynamodbScanOperator
+        public static string NOT_CONTAINS;
+        //
+        // Summary:
+        //     Constant NOT_NULL for DynamodbScanOperator
+        public static string NOT_NULL;
+        //
+        // Summary:
+        //     Constant NULL for DynamodbScanOperator
+        public static string NULL;
+    }
+}
